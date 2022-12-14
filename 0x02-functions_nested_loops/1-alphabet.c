f0d6ab1fd53f14ecc17986936ffc9fa2573f0424
Ivy N <ivystacyn@gmail.com>
2022-12-14 07:09:52 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet in lowercase
+ *
+ * Return: Always (0) Success
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
