d7535355440dcbd9ed814bbd9f066cb8b5c043da
Ivy N <ivystacyn@gmail.com>
2022-12-19 09:43:13 -0800

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
-include "main.h"
+#include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string
+ * starting with the first character
  *
  * @str: string parameter
  */
