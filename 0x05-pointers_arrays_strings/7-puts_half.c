bdfaeb971e2eb3ed3774c40ecb49786a86b8700d
Ivy N <ivystacyn@gmail.com>
2022-12-19 10:31:56 -0800

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,17 +8,17 @@ void puts_half(char *str)
 {
 	int l, n;
 
-	for (l = 0; str[l] != '/0'; ++l)
+	for (l = 0; str[l] != '\0'; ++l)
 		;
 
 	if (l % 2 == 0)
 	{
-		for (n = l / 2; strn[n] != '\0'; ++n)
+		for (n = l / 2; str[n] != '\0'; ++n)
 			_putchar(str[n]);
 	} else
 	{
-		for (n = ((l - 1) / 2 + 1; strn[n] != '\0'; ++n)
-				_putcahr(str[n]);
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
 	}
 	_putchar('\n');
 }
