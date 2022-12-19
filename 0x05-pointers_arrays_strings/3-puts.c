19b9850f3273b4b2a1359cf196f4c8291d10c5ce
Ivy N <ivystacyn@gmail.com>
2022-12-19 09:11:31 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string parameter
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(8str + 0);
+		++str;
+	}
+	_putchar('\n');
+}
