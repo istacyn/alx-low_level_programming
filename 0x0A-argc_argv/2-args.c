d827c29dc59cc08972dc063543522ed11f3bd221
Ivy N <ivystacyn@gmail.com>
2023-01-09 05:17:09 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints number of arguments passed into program
+ * @argc: number of arguments
+ * @argv: arrays of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
