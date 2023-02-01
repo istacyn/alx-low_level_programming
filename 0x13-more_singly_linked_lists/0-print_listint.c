9c5762c75c19f756304b1359389e971e90f5d73c
Ivy <ivystacyn@gmail.com>
2023-02-01 07:35:17 -0800

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: pointer to first node
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
+
+	printf("%d\n", h->n);
+
+	return (node_count);
+}
