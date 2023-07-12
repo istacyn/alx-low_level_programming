73f9c63ee6d8ceed71169fb17ee9c76405906a02
Ivy <ivystacyn@gmail.com>
2023-07-12 06:55:49 +0000

    Tasks 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -28,23 +28,14 @@ int binary_search(int *array, size_t size, int value)
 		mid = left + (right - left) / 2;
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
-		{
-			printf("%d ", array[i]);
-		}
-		printf("\n");
+			printf("%i%s", array[i], i == right ? "\n" : ", ");
 
-		if (array[mid] == value)
-		{
-			return (mid);
-		}
-		else if (array[mid] < value)
-		{
+		if (array[mid] < value)
 			left = mid + 1;
-		}
-		else
-		{
+		else if (array[mid] > value)
 			right = mid - 1;
-		}
+		else
+			return (mid);
 	}
 
 	return (-1);
